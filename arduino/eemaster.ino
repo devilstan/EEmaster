ff34587dc82ad0b7185d82eceb67c7fd1e452e5f
devilstan <devilstan@gmail.com>
2016-09-30 15:31:54 +0800

    Update eemaster.ino
    use uart0 to receive data from master side

--- arduino/eemaster.ino
+++ arduino/eemaster.ino
@@ -16,16 +16,34 @@
  http://www.arduino.cc/en/Tutorial/SerialEvent
 
  */
+ #include <SPI.h>
+#include <SD.h>
 
+File myFile;
 String inputString = "";         // a string to hold incoming data
+String inputStringB = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
+unsigned char sd_status;
+String filename;
 
 void setup() {
   // initialize serial:
   Serial.begin(19200, SERIAL_8N1);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
-  Serial.print("helloworld");
+
+  // SD card initialize
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done...");
+  sd_status = 0;
+  filename = "";
 }
 
 void loop() {
@@ -33,6 +51,49 @@ void loop() {
   // print the string when a newline arrives:
   if (stringComplete) {
     Serial.print(inputString);
+    switch (sd_status) {
+    case 0:
+      inputStringB = inputString;
+      inputStringB.trim();
+      if ( inputStringB == "write_file" ) {
+        Serial.println(" -> ready to write file");
+        sd_status = 1;
+      }
+      break;
+    case 1:
+      inputStringB = inputString;
+      inputStringB.trim();
+      filename = inputStringB;
+      if (filename != "") {
+        Serial.print("got the filename = ");
+        Serial.println(filename);
+        // open the file. note that only one file can be open at a time,
+        // so you have to close this one before opening another.
+        if (SD.exists(filename)) {
+          SD.remove(filename);
+        }
+        myFile = SD.open(filename, FILE_WRITE);
+      }
+      sd_status = 2;
+      break;
+    case 2: 
+      inputStringB = inputString;
+      inputStringB.trim();
+      if ( inputStringB != "EOF" ) {
+        myFile.println(inputStringB);  
+        Serial.print("OK");
+      }
+      else {
+        myFile.close();
+        Serial.println("done.");
+        sd_status = 3;  
+      }
+      break;
+     default:
+      Serial.print("sd_status = 0");
+      sd_status = 0;
+      break; 
+    }
     // clear the string:
     inputString = "";
     stringComplete = false;
@@ -58,5 +119,4 @@ void serialEvent() {
     }
   }
 }
-
 
